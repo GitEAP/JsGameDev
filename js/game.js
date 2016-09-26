2fa75421460d466f1d96624dfb6b5da1417baa5a
Erick Perez <NBA_EAP@hotmail.com>
2016-09-26 11:54:53 -0600

    added a function with return

--- js/game.js
+++ js/game.js
@@ -18,3 +18,13 @@ for (var i = 0; i < phrases.length; i++) {
     mainPlayer.messenger(phrases[i]);
 }
 
+function playGame(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+var firstPrompt = parseInt( prompt("Enter a number between 0 and 10") );
+var secondPrompt = parseInt( prompt("Enter a number between 11 and 20") );
+
+var score = playGame(firstPrompt, secondPrompt);
+alert("Your score is: " + score + ".");
+confirm("Do you want to play again?");
