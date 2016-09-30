155ee028cae9857492a51c7df33c7779a9302c5d
Erick Perez <NBA_EAP@hotmail.com>
2016-09-30 11:49:29 -0600

    2 button events addes

--- js/event.js
+++ js/event.js
@@ -1,15 +1,37 @@
-var myFunction = function myFunctionHandler() {
-    alert("Hello World");
+//UserName button
+var userName;
+
+var enterName = function myFunctionHandler() {
+    userName = prompt("Enter your name:");
 }
-var startGameHandler = function startMyGame () {
-   prompt("Enter your name:");
+//startGame Button
+var startGameHandler = function startMyGame() {
+    if (userName == undefined) {
+        alert("Please enter a valid name");
+    }
+    else {
+        alert("Ready to play.");
+
+    }
 }
 
 
-var button = document.getElementById("button1");
-
-button.addEventListener("click", myFunction);
+var button = document.getElementById("name");
+button.addEventListener("click", enterName);
 
 var startButton = document.getElementById("startGame");
-
 startButton.addEventListener("click", startGameHandler);
+
+
+
+var gameButton = function GameButtonConstruct(name) {
+    var button;
+    var fire = function () {
+        alert("Fire");
+    }
+};
+
+var fireWeaponButton = new gameButton();
+fireWeaponButton.button = document.getElementById("fireWeapon");
+console.log(fireWeaponButton.button);
+fireWeaponButton.button.addEventListener("click", fireWeaponButton.fire);
